c30f39d2c805e896ea2ac465842b46ffab4eabbb
lanyan-only <894468157@qq.com>
2016-03-21 09:07:32 +0800

    加链接

---
+++ js/index.js
@@ -0,0 +1,84 @@
+addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } 
+jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+     $( "span.menu" ).click(function() {
+		$( ".top-menu" ).slideToggle( "slow", function() {
+		// Animation complete.
+		});
+	});
+
+    var navoffeset=$(".header-bottom").offset().top;
+			 $(window).scroll(function(){
+				var scrollpos=$(window).scrollTop(); 
+				if(scrollpos >=navoffeset){
+					$(".header-bottom").addClass("fixed");
+				}else{
+					$(".header-bottom").removeClass("fixed");
+				}
+	 });
+
+    $('#dg-container').gallery({
+				autoplay	:	true
+	});
+    $("span.menu").click(function(){
+					$(" ul.navig").slideToggle("slow" , function(){
+					});
+				});
+    $("#slider4").responsiveSlides({
+			        auto: true,
+			        pager: true,
+			        nav: false,
+			        speed: 500,
+			        namespace: "callbacks",
+			        before: function () {
+			          $('.events').append("<li>before event fired.</li>");
+			        },
+			        after: function () {
+			          $('.events').append("<li>after event fired.</li>");
+			        }
+  });
+	$(".swipebox").swipebox();
+ //   var filterList = {
+	
+	// 	init: function () {
+		
+	// 		// MixItUp plugin
+	// 	// http://mixitup.io
+	// 	$('#portfoliolist').mixitup({
+	// 		targetSelector: '.portfolio',
+	// 		filterSelector: '.filter',
+	// 		effects: ['fade'],
+	// 		easing: 'snap',
+	// 		// call the hover effect
+	// 		onMixEnd: filterList.hoverEffect()
+	// 	});				
+	
+	// },
+	
+	// hoverEffect: function () {
+	
+	// 	// Simple parallax effect
+	// 	$('#portfoliolist .portfolio').hover(
+	// 		function () {
+	// 			$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+	// 			$(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');				
+	// 		},
+	// 		function () {
+	// 			$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+	// 			$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
+	// 		}		
+	// 	);				
+	
+	// }
+
+};
+
+// Run the show!
+	filterList.init();
+
+    $().UItoTop({ easingType: 'easeOutQuart' });
+    
+});
